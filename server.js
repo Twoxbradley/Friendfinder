b1d278df587f7954f7b254b7e7a40404b1a1d2fb
Twoxbradley <jimmybradley32@gmail.com>
2018-08-07 23:57:05 -0700

    improved functionality to form and have responsiveness to prompt ... haven't yet fixed form submission issue

--- server.js
+++ server.js
@@ -1,31 +1,37 @@
-// Dependencies
-// =============================================================
+// DEPENDENCIES
+// ==============================================================================
+
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require("path");
 
-// Sets up the Express App
-// =============================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
 var app = express();
 
-// Sets a initial port locally and creates a dynamic port in for horuku  
-var PORT = process.env.PORT || 3000;
+// Sets a dynamic port for Heroku in the event the local port isn't supported
+var PORT = process.env.PORT || 8080;
 
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: true }));
+// BodyParser makes it possible for our server to interpret data sent to it.
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use('/static', express.static(path.join(__dirname, 'app/public')))
+
 
 // ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-require("./Routing/apiRoutes")(app);
-require("./Routing/htmlRoutes")(app);
+require("./app/routing/api-routes.js")(app);
+require("./app/routing/html-routes.js")(app);
 
-// =============================================================================
 // LISTENER
-// The below code effectively "starts" our server
-// =============================================================================
+// ==============================================================================
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
